7117f473ce0023e484fa10be467b756cbd9521e4
Rakeshappu <103620757+Rakeshappu@users.noreply.github.com>
2026-05-31 17:02:39 +0530

    updated

--- data/configs/audit_table_ddl.sql
+++ data/configs/audit_table_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE avd-databricks-demo.temp_dataset.audit_log (
+CREATE TABLE healthcare-497406.temp_dataset.audit_log (
     data_source STRING,
     tablename STRING,
     load_type STRING,
